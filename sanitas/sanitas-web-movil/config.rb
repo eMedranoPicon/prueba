80ddd5d9fbf7f8431c8add4a9faeeea2f1c7edb8
msandoval-sopra <mesandoval@ITEM-66498.dhcp.vale.es.sopra>
2013-08-14 15:07:34 +0200

    [sanitas] reorganizacion carpetas. añadido padding pantalla busqueda-medica.html

--- sanitas/sanitas-web-movil/config.rb
+++ sanitas/sanitas-web-movil/config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
+css_dir = "css/site_sanitas_movil"
 sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "js"
+images_dir = "img/site_sanitas_movil"
+javascripts_dir = "js/site_sanitas_movil"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
