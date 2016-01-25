2a18ad54a5d2f91f26cd968a8d3de72fbcc23746
Gustavo Lanza Duarte <tavolanza@gmail.com>
2016-01-25 11:22:02 +0100

    Revert "Coinc2016 develop"

--- context_vars/context_list.py
+++
@@ -1,6 +0,0 @@
-import context_vars.index as index
-
-def get_context_list():
-    return [
-    	('index.html', index.get_index),
-    ]
--- script/deployApp.sh
+++
@@ -1,2 +0,0 @@
-scp -r app/dist root@188.226.223.252:/var/www/html/temp-deusto
-ssh root@188.226.223.252 'cd /var/www/html; rm -rf deusto; mv temp-deusto deusto; exit;'
