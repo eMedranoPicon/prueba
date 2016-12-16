8196e29fbf5528f9bdd2b3f992c7f767003e2300
Jacobo <jacobo.segovia93@gmail.com>
2016-12-16 14:11:52 +0100

    remove py vars

--- context_vars/context_list.py
+++ context_vars/context_list.py
@@ -1,10 +1,2 @@
-import context_vars.index as index;
-import context_vars.warning as warning;
-
 def get_context_list():
-    return [
-    	('index.html', index.get),
-    	('warning1.html', warning.get),
-    	('warning2.html', warning.get),
-    	('warning3.html', warning.get),
-    ]
+    return []
--- context_vars/warning.py
+++
@@ -1,4 +0,0 @@
-def get():
-	return {
-		"title": "Verificar tarjeta"
-	}
