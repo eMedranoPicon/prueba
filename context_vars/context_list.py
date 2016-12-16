9e33d3b257b33dd763a425cc6e2a865894d66f3b
Jacobo <jacobo.segovia93@gmail.com>
2016-12-16 13:23:58 +0100

    create all tempaltes of warning messages

--- context_vars/context_list.py
+++ context_vars/context_list.py
@@ -1,8 +1,10 @@
 import context_vars.index as index;
-import context_vars.warning1 as warning1;
+import context_vars.warning as warning;
 
 def get_context_list():
     return [
     	('index.html', index.get),
-    	('warning1.html', warning1.get),
+    	('warning1.html', warning.get),
+    	('warning2.html', warning.get),
+    	('warning3.html', warning.get),
     ]
