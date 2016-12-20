0081ecfcff722db94b93e56c538a408f0ae6421d
Jacobo <jacobo.segovia93@gmail.com>
2016-12-20 11:27:59 +0100

    change script to upload

--- script/deployApp.sh
+++ script/deployApp.sh
@@ -1,2 +1,2 @@
-scp -r app/dist root@188.226.223.252:/var/www/html/temp-deusto
-ssh root@188.226.223.252 'cd /var/www/html; rm -rf deusto; mv temp-deusto deusto; exit;'
+scp -r app/dist root@188.226.223.252:/var/www/html/temp-coinc3Dsecure
+ssh root@188.226.223.252 'cd /var/www/html; rm -rf coinc3Dsecure; mv temp-coinc3Dsecure coinc3Dsecure; exit;'
