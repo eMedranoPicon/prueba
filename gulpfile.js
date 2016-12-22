642dd064b487fc2e9162444db34b827afc645df3
Jacobo <jacobo.segovia93@gmail.com>
2016-12-22 09:03:04 +0100

    add task to create release

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,56 @@
 
-var gulp = require('gulp');
-var path = require('path');
-var jshint = require('gulp-jshint');
-var autoprefixer = require('gulp-autoprefixer');
-var sass = require('gulp-sass');
-var minifycss = require('gulp-minify-css');
-var runSequence = require('run-sequence');
-var concat = require('gulp-concat');
-var shell = require('gulp-shell');
-var connect = require('gulp-connect');
-var notify = require('gulp-notify');
-var del = require('del');
-var uglify = require('gulp-uglify');
+var gulp            = require('gulp');
+var path            = require('path');
+var jshint          = require('gulp-jshint');
+var autoprefixer    = require('gulp-autoprefixer');
+var sass            = require('gulp-sass');
+var minifycss       = require('gulp-minify-css');
+var runSequence     = require('run-sequence');
+var concat          = require('gulp-concat');
+var shell           = require('gulp-shell');
+var connect         = require('gulp-connect');
+var notify          = require('gulp-notify');
+var del             = require('del');
+var uglify          = require('gulp-uglify');
+var zip             = require('gulp-zip');
+var argv            = require('yargs').argv;
+var prompt          = require('gulp-prompt');
+var release         = require('gulp-github-release');
 
 
 
-var config_dir = './config';
-var src_app   = './app';
-var src_assets   = path.join(src_app, 'assets');
-var src_vendor  = path.join(src_assets, 'vendor');
-var src_dist  = path.join(src_app, 'dist');
+var config_dir      = './config';
+var src_app         = './app';
+var src_assets      = path.join(src_app, 'assets');
+var src_vendor      = path.join(src_assets, 'vendor');
+var src_dist        = path.join(src_app, 'dist');
 
 
-var src_js_old_browsers = path.join(src_vendor, 'js/old_browsers_support', '**', '*');
-var src_js_files = path.join(src_assets, 'js', '**', '*.js');
-var src_sass_desktop = path.join(src_assets, 'sass', 'styles-desktop.scss');
-var src_sass_phone = path.join(src_assets, 'sass', 'styles-phone.scss');
-var src_sass = path.join(src_assets, 'sass', '**', '*.scss');
-var src_js = path.join(src_assets, 'js', '**', '*.js');
-var src_fonts = path.join(src_assets, 'fonts', '**');
-var src_images = path.join(src_assets, 'images', '**');
-var src_html_files = path.join(src_app, 'templates', '**', '*.html');
+var src_js_old_browsers     = path.join(src_vendor, 'js/old_browsers_support', '**', '*');
+var src_js_files            = path.join(src_assets, 'js', '**', '*.js');
+var src_sass_desktop        = path.join(src_assets, 'sass', 'styles-desktop.scss');
+var src_sass_phone          = path.join(src_assets, 'sass', 'styles-phone.scss');
+var src_sass                = path.join(src_assets, 'sass', '**', '*.scss');
+var src_js                  = path.join(src_assets, 'js', '**', '*.js');
+var src_fonts               = path.join(src_assets, 'fonts', '**');
+var src_images              = path.join(src_assets, 'images', '**');
+var src_html_files          = path.join(src_app, 'templates', '**', '*.html');
 
-var src_dist_js_old_browsers = 'js/old_browsers_support';
-var src_dist_css = 'css';
-var src_dist_js = 'js';
-var src_dist_fonts = 'fonts';
-var src_dist_images = 'images';
-var src_dist_desktop  = path.join(src_dist, 'desktop');
-var src_dist_phone  = path.join(src_dist, 'phone');
+var src_dist_js_old_browsers    = 'js/old_browsers_support';
+var src_dist_css                = 'css';
+var src_dist_js                 = 'js';
+var src_dist_fonts              = 'fonts';
+var src_dist_images             = 'images';
+var src_dist_desktop            = path.join(src_dist, 'desktop');
+var src_dist_phone              = path.join(src_dist, 'phone');
 
-var vendor_source_maps = [
+var release_base_dir = './release';
+
+var vendor_source_maps  = [
     './bower_components/jquery/dist/jquery.min.map',
     './bower_components/progressbar.js/dist/progressbar.min.js.map'
 ];
-var vendor_js_src = [
+var vendor_js_src       = [
     './bower_components/jquery/jquery.min.js',
     './bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.min.js',
     './bower_components/jquery-ui/jquery-ui.min.js'
@@ -197,6 +203,57 @@ gulp.task('connect', function() {
 });
 
 
+
+gulp.task('create-release-zip', function() {
+    return gulp
+        .src(path.join(src_dist, '**', '*'), argv)
+        .pipe(zip(release_version + '.zip'))
+        .pipe(gulp.dest(release_base_dir));
+}).help = 'Create a zip with contains the release';
+
+gulp.task('upload-release', function() {
+    gulp
+    .src('')
+    .pipe(prompt
+        .prompt([{
+            type: 'input',
+            name: 'token',
+            message: 'Please, insert a token string.'
+        }], function(res){
+            gulp
+                .src([path.join(release_base_dir, release_version + '.zip')])
+                .pipe(release({
+                    token: res.token,
+                    tag: release_version,
+                    manifest: require('./package.json')
+                }));
+        })
+    );
+}).help = 'Upload the release to the repository';
+
+gulp.task('create-release', function(cb) {
+    if (!argv.release) {
+        return;
+    }
+    del([release_base_dir], cb);
+    gulp
+        .src('')
+        .pipe(prompt
+            .prompt([{
+                type: 'input',
+                name: 'version',
+                message: 'Please, insert the version of release (v0.0.0).'
+            }], function(res){
+                release_version = res.version;
+                runSequence(
+                    ['create-release-zip'],
+                    ['upload-release']
+                );
+            })
+        );
+}).help = 'Create and complete all process to upload a release on the repository';
+
+
 gulp.task('default', function() {
     runSequence(
         ['old_browsers', 'jshint-dist', 'fonts', 'images', 'sass', 'vendor-scripts', 'scripts', 'jinja'],
@@ -215,6 +272,7 @@ gulp.task('dist', function() {
     runSequence(
         'clean',
         ['old_browsers', 'jshint-dist', 'fonts', 'images', 'sass-min', 'vendor-scripts', 'scripts-min', 'jinja'],
+        'create-release', // if has tag --release , create and upload the release, else pass
         function() {
             gulp.src('').pipe(notify({
                 title: 'Dist',
